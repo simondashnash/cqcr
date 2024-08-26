6e3d80419df1e2c162857f393c989a32fb22a96b
simondashnash <4538395+simondashnash@users.noreply.github.com>
2024-08-26 10:21:31 +0100

    removed commented out old function

--- R/cqcr-package.R
+++ R/cqcr-package.R
@@ -34,15 +34,3 @@ NULL
 }
 invisible()
 
-# .onLoad <- function(libname, pkgname) {
-#   if (is.null(getOption("cqc.partner.code"))) {
-#     key <- Sys.getenv("CQC_PARTNER_CODE")
-#     if (key != "") {
-#       if (substr(key, 1, 13) != "&partnerCode=") {
-#         key <- paste0("&partnerCode=", key)
-#       }
-#       options("cqc.partner.code" = key)
-#     }
-#   }
-#   invisible()
-# }
