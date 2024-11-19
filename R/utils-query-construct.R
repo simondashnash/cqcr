75671acf28169ce16c8befa5e3e04555f083f4e5
simondashnash <4538395+simondashnash@users.noreply.github.com>
2024-11-19 13:25:48 +0000

    Better handling of errors on get details.

--- R/utils-query-construct.R
+++ R/utils-query-construct.R
@@ -6,8 +6,7 @@ cqc_query_construction <- function(query) {
   if (is.null(subscription_key)) {
     stop("CQC Subscription Key not set. The API will reject your request")
   } else if (!grepl("&", query)) {
-    print("we were trying to put a spare ? here")
-    #query <- paste0(query, "?")
+    #do nothing
   }
 
   query2 <- paste0(
@@ -15,17 +14,39 @@ cqc_query_construction <- function(query) {
   )
 
 
+
+  # Make the GET request
   x <- httr::GET(query2, add_headers(`Ocp-Apim-Subscription-Key` = subscription_key))
 
+
   if (httr::status_code(x) != "200") {
-    stop(paste(
-      "Request returned error code:",
-      httr::status_code(x)
-    ), call. = FALSE)
-  }
+      # Log the error message and URL or any other useful information
+      error_message <- paste("Request returned error code:", httr::status_code(x))
+      request_url <- query2  # or use x$url if available
 
-  suppressMessages(cont <- jsonlite::fromJSON(httr::content(x, "text")))
+      # If the cqcr_errors data frame does not exist, create it
+      if (!exists("cqcr_errors")) {
+        cqcr_errors <- data.frame(
+          error_message = character(),
+          url = character(),
+          stringsAsFactors = FALSE
+        )
+      }
 
-  cont
+      # Append the error message and URL to the cqcr_errors data frame
+      cqcr_errors <- rbind(cqcr_errors, data.frame(
+        error_message = error_message,
+        url = request_url,
+        stringsAsFactors = FALSE
+      ))
+
+    }
+    else {
+      # Proceed with fromJSON only if the request was successful
+      suppressMessages(cont <- jsonlite::fromJSON(httr::content(x, "text")))
+
+      # Return the parsed content
+      cont
+    }
 }
 
