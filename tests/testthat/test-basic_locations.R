51213901a57e7eff4d3f4da0005dc55b0e58b9a9
simondashnash <4538395+simondashnash@users.noreply.github.com>
2025-04-13 09:40:11 +0100

    invalid location returns a df with error message, test now looks for this

--- tests/testthat/test-basic_locations.R
+++ tests/testthat/test-basic_locations.R
@@ -1,14 +1,20 @@
 test_that("basic locations works", {
   skip_on_cran()
 
+  # valid location
   location1 <- cqc_location("1-1428643301")
   expect_true(is.list(location1))
 
-  expect_error(cqc_location("blaah"))
-
+  # valid location inspection area
   insp_loc1 <- cqc_location_inspection_area("1-122710325")
-
   expect_true(is.list(insp_loc1))
+
+  # Invalid location returns error data frame
+  error_result <- cqc_location("blaah")
+  expect_s3_class(error_result, "data.frame")
+  expect_true(all(c("error_message", "url") %in% colnames(error_result)))
+  expect_type(error_result$error_message, "character")
+  expect_gt(nchar(error_result$error_message[1]), 0)
 
   options("cqc.subscription.key" = NULL) # Clear the option after the test
 })
